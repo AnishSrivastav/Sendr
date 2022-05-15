9b7d365f46517c16c07611e98ed2c5c4804902f2
eknoor2001 <200020051@iitb.ac.in>
2022-05-15 12:39:55 +0530

    compilable code v0

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,7 +22,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  // solidity: "0.8.4",
+  contractSizer: {
+    alphaSort: false,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
